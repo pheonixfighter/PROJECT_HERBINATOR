c8b5c075a4410a8f1411188275baed8e3ea8b79c
sudoweilder <robertec2007@gmail.com>
2026-04-17 15:21:13 -0700

    Added some python outline

---
+++ Python/herbinator.py
@@ -0,0 +1,22 @@
+# April 2025
+# This file holds the code for the communications functions for the 
+# UWT Project Herbinator's communications file
+
+
+
+
+def send_command():
+    print("I am working on it")
+
+
+def change_herbnet_name():
+    print("Needs to be done")
+
+def change_herbinator_name():
+    print("to be worked on")
+
+def herbinator_water():
+    print("to be worked on")
+
+def pause_herbinator():
+    print("outline function")
---
+++ Python/interface.py
@@ -0,0 +1,16 @@
+# This file contains the code for a CMD line python interface for project herbinator
+# The interface is going to be a temporary replacement for the GUI that will be programmed
+
+
+def main():
+    print("Welcome to project herbinator!")
+
+    while (True):
+        print("I am a dwarf and I'm digging a hole")
+        break
+
+
+def print_menu():
+    print("1. List herbinators")
+    print("2. Pause herbinator")
+    print("3. Force herbinator watering")
